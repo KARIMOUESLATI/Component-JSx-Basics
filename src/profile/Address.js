e5364b08a69fc8caab41c52b02ddc6ed245d8cd9
karim oueslati <karimoueslati16@gmail.com>
2020-08-10 10:54:19 +0200

    commit

---
+++ src/profile/Address.js
@@ -0,0 +1,7 @@
+import React from "react" ;
+import '../App.css' ;
+
+
+const Adress = () =>{return <p> Adress : Rue de tanit les jardins de carthage</p> ;} ;
+
+export default Adress ;
---
+++ src/profile/FullName.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css' ;
+
+
+
+const FullName = ()=> {
+    return (
+      <div className="FullName">
+          <h1>My name: Karim Oueslati</h1>
+        
+        
+          
+        
+      </div>
+    );
+  };
+  
+  export default FullName;
